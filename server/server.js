4028214418efa1f4708cc0448f3533f0ad7ff7f2
Antonio Moretti <amoretti86@gmail.com>
2025-03-25 12:07:57 -0400

    downgrade squres bitch ass

--- server/server.js
+++ server/server.js
@@ -3,7 +3,8 @@ const path = require('path');
 const { createClient } = require('@supabase/supabase-js');
 require('dotenv').config();
 const multer = require('multer');
-const { Client, Environment } = require('square');
+const { ApiError, Client, Environment } = require('square');
+
 
 
 const { randomUUID } = require('crypto');
@@ -15,9 +16,12 @@ const cors = require('cors');
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE_KEY);
 
 // Initialize Square client
+
 const squareClient = new Client({
   accessToken: process.env.SQUARE_ACCESS_TOKEN,
-  environment: process.env.NODE_ENV === 'production' ? Environment.Production : Environment.Sandbox,
+  environment: process.env.NODE_ENV === 'production' 
+    ? Environment.Production 
+    : Environment.Sandbox
 });
 // Configure email transporter (if using)
 /*
